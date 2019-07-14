ac24ce08783c61998642934256b5f79d0c1fc028
django-wong <me@djangowong.com>
2019-07-14 21:36:47 +0800

    replace react with vue

--- example/renderer.js
+++ example/renderer.js
@@ -1,128 +1,138 @@
 "use strict";
 
+const Vue = require("vue/dist/vue");
 const path = require("path");
-const React = require("react");
-const ReactDOM = require("react-dom");
 const {remote} = require("electron");
-const {ReactMPV} = require("../index");
+const {VueMPV} = require("../index");
 
-class Main extends React.PureComponent {
-  constructor(props) {
-    super(props);
-    this.mpv = null;
-    this.state = {pause: true, "time-pos": 0, duration: 0, fullscreen: false};
-    this.handleKeyDown = this.handleKeyDown.bind(this);
-    this.handleMPVReady = this.handleMPVReady.bind(this);
-    this.handlePropertyChange = this.handlePropertyChange.bind(this);
-    this.toggleFullscreen = this.toggleFullscreen.bind(this);
-    this.togglePause = this.togglePause.bind(this);
-    this.handleStop = this.handleStop.bind(this);
-    this.handleSeek = this.handleSeek.bind(this);
-    this.handleSeekMouseDown = this.handleSeekMouseDown.bind(this);
-    this.handleSeekMouseUp = this.handleSeekMouseUp.bind(this);
-    this.handleLoad = this.handleLoad.bind(this);
-  }
-  componentDidMount() {
-    document.addEventListener("keydown", this.handleKeyDown, false);
-  }
-  componentWillUnmount() {
-    document.removeEventListener("keydown", this.handleKeyDown, false);
-  }
-  handleKeyDown(e) {
-    e.preventDefault();
-    if (e.key === "f" || (e.key === "Escape" && this.state.fullscreen)) {
-      this.toggleFullscreen();
-    } else if (this.state.duration) {
-      this.mpv.keypress(e);
-    }
-  }
-  handleMPVReady(mpv) {
-    this.mpv = mpv;
-    const observe = mpv.observe.bind(mpv);
-    ["pause", "time-pos", "duration", "eof-reached"].forEach(observe);
-    this.mpv.property("hwdec", "auto");
-    this.mpv.command("loadfile", path.join(__dirname, "tos.mkv"));
-  }
-  handlePropertyChange(name, value) {
-    if (name === "time-pos" && this.seeking) {
-      return;
-    } else if (name === "eof-reached" && value) {
-      this.mpv.property("time-pos", 0);
-    } else {
-      this.setState({[name]: value});
-    }
-  }
-  toggleFullscreen() {
-    if (this.state.fullscreen) {
-      document.webkitExitFullscreen();
-    } else {
-      this.mpv.fullscreen();
-    }
-    this.setState({fullscreen: !this.state.fullscreen});
-  }
-  togglePause(e) {
-    e.target.blur();
-    if (!this.state.duration) return;
-    this.mpv.property("pause", !this.state.pause);
-  }
-  handleStop(e) {
-    e.target.blur();
-    this.mpv.property("pause", true);
-    this.mpv.command("stop");
-    this.setState({"time-pos": 0, duration: 0});
-  }
-  handleSeekMouseDown() {
-    this.seeking = true;
-  }
-  handleSeek(e) {
-    e.target.blur();
-    const timePos = +e.target.value;
-    this.setState({"time-pos": timePos});
-    this.mpv.property("time-pos", timePos);
-  }
-  handleSeekMouseUp() {
-    this.seeking = false;
-  }
-  handleLoad(e) {
-    e.target.blur();
-    const items = remote.dialog.showOpenDialog({filters: [
-      {name: "Videos", extensions: ["mkv", "webm", "mp4", "mov", "avi"]},
-      {name: "All files", extensions: ["*"]},
-    ]});
-    if (items) {
-      this.mpv.command("loadfile", items[0]);
-    }
-  }
-  render() {
-    return (
-      <div className="container">
-        <ReactMPV
-          className="player"
-          onReady={this.handleMPVReady}
-          onPropertyChange={this.handlePropertyChange}
-          onMouseDown={this.togglePause}
-        />
-        <div className="controls">
-          <button className="control" onClick={this.togglePause}>
-            {this.state.pause ? "▶" : "❚❚"}
-          </button>
-          <button className="control" onClick={this.handleStop}>■</button>
-          <input
-            className="seek"
-            type="range"
-            min={0}
-            step={0.1}
-            max={this.state.duration}
-            value={this.state["time-pos"]}
-            onChange={this.handleSeek}
-            onMouseDown={this.handleSeekMouseDown}
-            onMouseUp={this.handleSeekMouseUp}
-          />
-          <button className="control" onClick={this.handleLoad}>⏏</button>
-        </div>
-      </div>
+
+const app = new Vue({
+  data() {
+    return {
+      fullscreen: false,
+      pause: true,
+      "time-pos": 0,
+      duration: 0,
+      styles: {
+        backgroundColor: "red",
+      },
+    };
+  },
+  el: "#main",
+  template: `#content`,
+  components: {
+    "vue-mpv": VueMPV,
+  },
+
+  mounted() {
+    document.addEventListener(
+      "keydown",
+      this.handleKeyDown,
+      false
     );
-  }
-}
+  },
 
-ReactDOM.render(<Main/>, document.getElementById("main"));
+  beforeDestroy() {
+    document.removeEventListener(
+      "keydown",
+      this.handleKeyDown,
+      false
+    );
+  },
+
+  methods: {
+    handleKeyDown(e) {
+      e.preventDefault();
+      if (e.key === "f" || (e.key === "Escape" && this.$data.fullscreen)) {
+        this.toggleFullscreen();
+      } else if (this.$data.duration) {
+        this.mpv.keypress(e);
+      }
+    },
+
+    handleMPVReady(mpv) {
+      this.mpv = mpv;
+      const observe = mpv.observe.bind(mpv);
+      ["pause", "time-pos", "duration", "eof-reached"].forEach(observe);
+      this.mpv.property("hwdec", "auto");
+      this.mpv.command("loadfile", path.join(__dirname, "tos.mkv"));
+    },
+
+    onReady(mpv) {
+      this.mpv = mpv;
+      const observe = mpv.observe.bind(mpv);
+      ["pause", "time-pos", "duration", "eof-reached"].forEach(observe);
+      this.mpv.property("hwdec", "auto");
+      this.mpv.command("loadfile", path.join(__dirname, "tos.mkv"));
+    },
+
+    handlePropertyChange(name, value) {
+      if (name === "time-pos" && this.seeking) {
+        return;
+      } else if (name === "eof-reached" && value) {
+        this.mpv.property("time-pos", 0);
+      } else {
+        this.$data[name] = value;
+      }
+    },
+
+    toggleFullscreen() {
+      if (this.$data.fullscreen) {
+        document.webkitExitFullscreen();
+      } else {
+        this.mpv.fullscreen();
+      }
+      this.$data.fullscreen = !this.$data.fullscreen;
+    },
+
+    togglePause(e) {
+      e.target.blur();
+      if (!this.$data.duration) return;
+      this.mpv.property("pause", !this.$data.pause);
+    },
+
+    handleStop(e) {
+      e.target.blur();
+      this.mpv.property("pause", true);
+      this.mpv.command("stop");
+      this.$data["time-pos"] = 0;
+      this.$data.duration = 0;
+    },
+
+    handleSeekMouseDown() {
+      this.seeking = true;
+    },
+
+    handleSeek(e) {
+      e.target.blur();
+      const timePos = +e.target.value;
+      this.$data["time-pos"] = timePos;
+      this.mpv.property("time-pos", timePos);
+    },
+
+    handleSeekMouseUp() {
+      this.seeking = false;
+    },
+
+    handleLoad(e) {
+      e.target.blur();
+      const items = remote.dialog.showOpenDialog({filters: [
+        {name: "Videos", extensions: ["mkv", "webm", "mp4", "mov", "avi"]},
+        {name: "All files", extensions: ["*"]},
+      ]});
+      if (items) {
+        this.mpv.command("loadfile", items[0]);
+      }
+    },
+  },
+
+
+  computed: {
+    timePos() {
+      return this.$data["time-pos"];
+    },
+  },
+});
+
+
+console.info(app);
